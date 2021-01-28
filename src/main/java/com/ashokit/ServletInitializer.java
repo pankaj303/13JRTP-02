89fe964c06478c3fd485edbd968309d7ed1c51d7
Pankaj Tiwari <you@example.com>
2021-01-28 06:20:09 +0530

    added changes

--- src/main/java/com/ashokit/ServletInitializer.java
+++ src/main/java/com/ashokit/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	public static final url = "www.amazon.com";
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
